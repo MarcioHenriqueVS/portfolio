94620154449a18d23d7af44079524db29e8647d8
marcio <marciohvalingmail.com>
2024-11-15 14:43:21 -0300

    corrigindo tamanho das fontes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2145,14 +2145,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "314950459bbd35ed4a86e2497e22720d
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "68fe44da61dfe2dd99a738359bf1ca7b",
+"flutter_bootstrap.js": "36f364c792ce1d006333605f7172908a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c1c5f29031926111ab3ccb7c551c88f9",
 "/": "c1c5f29031926111ab3ccb7c551c88f9",
-"main.dart.js": "3fa892590fea46f5d87bc4ade2a58bc2",
+"main.dart.js": "c33f7a1f7fa16c99d956ad110f0f0f61",
 "manifest.json": "d40c47d1c161f94dbcb13094d37f1f55",
 "version.json": "009c9e65172e010890f7f65fde438006"};
 // The application shell files that are downloaded before a service worker can
